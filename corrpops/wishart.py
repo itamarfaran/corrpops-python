da7c6c1a3f3c8b5d5be95c325b25803c729c74e4
Itamar Faran <itamar@fidocredit.com>
2024-12-30 03:15:06 +0200

    add arma_wishart_rvs

--- corrpops/wishart.py
+++ corrpops/wishart.py
@@ -1,4 +1,5 @@
-from typing import Any, Union, Tuple
+import copy
+from typing import Any, Union, Tuple, Iterable
 import numpy as np
 from scipy import stats
 from utils import is_positive_definite
@@ -7,7 +8,7 @@ from utils import is_positive_definite
 def generalized_wishart_rvs(
         df: int,
         scale: np.ndarray,
-        size: Union[int, Tuple[int]] = 1,
+        size: Union[int, Tuple[int, ...]] = 1,
         random_state: Any = None,
 ) -> np.ndarray:
     if isinstance(size, int):
@@ -25,16 +26,53 @@ def generalized_wishart_rvs(
 
 def is_invertible_arma(coefs, tol=1e-03):
     x = np.linspace(-1, 1, int(2 / tol))[:, None]
-    results = 1 - np.sum(coefs * x ** (1 + np.arange(len(coefs))), axis=1)
+    results = 1 - np.sum(np.asarray(coefs) * x ** (1 + np.arange(len(coefs))), axis=1)
     return (results > 0).all() or (results < 0).all()
 
 
 def arma_wishart_rvs(
         df: int,
         scale: np.ndarray,
-        ar: Union[float, Tuple[float]] = None,
-        ma: Union[float, Tuple[float]] = None,
-        size: Union[int, Tuple[int]] = 1,
+        ar: Union[float, Iterable[float]] = None,
+        ma: Union[float, Iterable[float]] = None,
+        size: Union[int, Tuple[int, ...]] = 1,
         random_state: Any = None,
 ):
-    pass
+    if ar is None:
+        ar = []
+    elif not isinstance(ar, (tuple, list)):
+        ar = [ar]
+    else:
+        ar = copy.copy(list(ar))
+    if ma is None:
+        ma = []
+    elif not isinstance(ma, (tuple, list)):
+        ma = [ma]
+    else:
+        ma = copy.copy(list(ma))
+
+    if not is_invertible_arma(ar):
+        raise ValueError
+    if not is_invertible_arma(ma):
+        raise ValueError
+    if isinstance(size, int):
+        size = (size,)
+
+    while len(ma) < len(ar):
+        ma.append(0)
+    while len(ar) < len(ma):
+        ar.append(0)
+
+    max_lag = len(ar)
+
+    eps = stats.multivariate_normal.rvs(None, scale, size + (df,), random_state)
+    out = np.zeros(size + (df, scale.shape[-1]), float)
+    for i in range(df):
+        out[..., i, :] = eps[..., i, :]
+
+        for lag in range(max_lag):
+            if lag < i:
+                out[..., i, :] += ma[lag] * eps[..., i - lag - 1, :]
+                out[..., i, :] += ar[lag] * out[..., i - lag - 1, :]
+
+    return np.swapaxes(out, -1, -2) @ out

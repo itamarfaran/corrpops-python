f924888dbdb8335b3eb42237a2afa37e7d830978
Itamar Faran <itamar@fidocredit.com>
2024-12-30 03:56:06 +0200

    add requires

--- setup.py
+++ setup.py
@@ -7,4 +7,8 @@ setup(
     ext_modules=cythonize("corrpops/corr_calc_cy.pyx"),
     include_dirs=[numpy.get_include()],
     compiler_directives={"language_level": "3"},
+    install_requires=[
+        'numpy>=1.20.0',
+        'scipy>=1.6.0,',
+    ]
 )

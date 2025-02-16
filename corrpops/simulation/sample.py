8baa8b81591858c7b7dbea907abb5db446250f75
Itamar Faran <itamar@fidocredit.com>
2025-02-17 01:27:24 +0200

    add sample

---
+++ corrpops/simulation/sample.py
@@ -0,0 +1,126 @@
+from typing import Tuple
+from model.link_functions import MultiplicativeIdentity
+import numpy as np
+from scipy import stats
+from utils.matrix import force_symmetry, cov_to_corr
+from simulation.wishart import arma_wishart_rvs
+from utils.triangle_vector import triangle_to_vector
+
+
+def build_parameters(
+    p: int,
+    percent_alpha: float,
+    alpha_min: float,
+    alpha_max: float,
+    dim_alpha: int = 1,
+    theta_loc: float = 0.0,
+    theta_scale: float = 1.0,
+    enforce_min_alpha: bool = False,
+    seed=None,
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    rng = np.random.default_rng(seed)
+
+    theta = stats.norm.rvs(
+        loc=theta_loc,
+        scale=theta_scale,
+        size=(2 * p, p),
+        random_state=rng,
+    )
+    theta = force_symmetry(cov_to_corr(theta.T @ theta))
+    stds = np.diag(np.abs(stats.logistic.rvs(size=p, random_state=rng)))
+    sigma = stds @ theta @ stds
+
+    n_non_null_alphas = int(percent_alpha * p)
+    non_null_alphas = rng.choice(p, n_non_null_alphas, replace=False)
+
+    alpha_row_sums = np.ones(p)
+    alpha_row_sums[non_null_alphas] = stats.uniform.rvs(
+        loc=alpha_min,
+        scale=alpha_max - alpha_min,
+        size=n_non_null_alphas,
+        random_state=rng,
+    )
+    if enforce_min_alpha:
+        alpha_row_sums[rng.choice(non_null_alphas)] = alpha_min
+
+    alpha = stats.uniform.rvs(size=(dim_alpha, p), random_state=rng)
+    alpha /= alpha.sum(axis=0)  # normalize rows to 1
+    alpha *= alpha_row_sums  # reduce row sums to alpha_row_sums
+    return theta, alpha.T, sigma
+
+
+def create_samples(
+    control_n,
+    diagnosed_n,
+    control_correlation,
+    diagnosed_correlation,
+    t_length=100,
+    control_ar=None,
+    control_ma=None,
+    diagnosed_ar=None,
+    diagnosed_ma=None,
+    size=1,
+    random_effect=None,
+    seed=None,
+):
+    rng = np.random.default_rng(seed)
+    p = control_correlation.shape[-1]
+
+    control = arma_wishart_rvs(
+        df=t_length,
+        scale=control_correlation,
+        ar=control_ar,
+        ma=control_ma,
+        size=control_n * size,
+        random_state=rng,
+    ).reshape(size, control_n, p, p)
+
+    diagnosed = arma_wishart_rvs(
+        df=t_length,
+        scale=diagnosed_correlation,
+        ar=diagnosed_ar,
+        ma=diagnosed_ma,
+        size=diagnosed_n * size,
+        random_state=rng,
+    ).reshape(size, diagnosed_n, p, p)
+
+    control = force_symmetry(cov_to_corr(control))
+    diagnosed = force_symmetry(cov_to_corr(diagnosed))
+
+    return control, diagnosed
+
+
+def create_samples_from_parameters(
+    control_n,
+    diagnosed_n,
+    theta,
+    alpha,
+    link_function,
+    t_length=100,
+    control_ar=None,
+    control_ma=None,
+    diagnosed_ar=None,
+    diagnosed_ma=None,
+    size=1,
+    random_effect=None,
+    seed=None,
+):
+    g11 = link_function.func(
+        t=triangle_to_vector(theta),
+        a=alpha,
+        d=alpha.shape[-1],
+    )
+    return create_samples(
+        control_n=control_n,
+        diagnosed_n=diagnosed_n,
+        control_correlation=theta,
+        diagnosed_correlation=g11,
+        t_length=t_length,
+        control_ar=control_ar,
+        control_ma=control_ma,
+        diagnosed_ar=diagnosed_ar,
+        diagnosed_ma=diagnosed_ma,
+        size=size,
+        random_effect=random_effect,
+        seed=seed,
+    )
--- debug.py
+++ debug.py
@@ -1,37 +1,40 @@
-import numpy as np
-from simulation.wishart import generalized_wishart_rvs
-from utils.matrix import cov_to_corr
 from model.estimator import CorrPopsEstimator
 from model.optimizer import CorrPopsOptimizer
+from model.link_functions import MultiplicativeIdentity
+from simulation.sample import build_parameters, create_samples_from_parameters
 
-
-p_ = 0.5
-m_ = 4
-shape = p_ * np.ones((m_, m_)) + (1 - p_) * np.eye(m_)
-
-
-diagnosed = generalized_wishart_rvs(
-    100,
-    shape,
-    size=200,
+theta, alpha, _ = build_parameters(
+    p=10,
+    percent_alpha=0.3,
+    dim_alpha=1,
+    alpha_min=0.7,
+    alpha_max=0.9,
 )
-control = generalized_wishart_rvs(
-    100,
-    shape,
-    size=200,
+control, diagnosed = create_samples_from_parameters(
+    control_n=19,
+    diagnosed_n=12,
+    theta=theta,
+    alpha=alpha,
+    link_function=MultiplicativeIdentity(),
+    t_length=100,
+    control_ar=[0.5, 0.2],
+    control_ma=0.2,
+    diagnosed_ar=[0.5, 0.2],
+    diagnosed_ma=0.2,
+    size=1,
+    random_effect=None,
+    seed=12,
 )
-
-diagnosed = cov_to_corr(diagnosed)
-control = cov_to_corr(control)
 
 
 model = CorrPopsEstimator(
     # link_function=MultiplicativeIdentity(transformer=Transformer(np.log, np.exp))
     optimizer=CorrPopsOptimizer(verbose=True),
 )
-model.fit(control, diagnosed)
+model.fit(control[0], diagnosed[0])
 print(
-    model.naive_optimizer.alpha_.round(2),
-    model.alpha_.round(2),
+    alpha.flatten().round(2), "\n",
+    model.naive_optimizer.alpha_.round(2), "\n",
+    model.alpha_.round(2), "\n",
 )
-pass
+

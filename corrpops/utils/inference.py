e987a32ec22de74954b323e23b254fbaa391488c
Itamar Faran <itamar@fidocredit.com>
2025-02-17 16:51:14 +0200

    add inference.py

---
+++ corrpops/utils/inference.py
@@ -0,0 +1,66 @@
+from typing import Dict, Literal
+
+import numpy as np
+from scipy import stats
+
+from utils.triangle_vector import triangle_to_vector
+
+
+def piece_wise_comparison(
+    control: np.ndarray,
+    diagnosed: np.ndarray,
+    p_adjust_method: str = None,
+    alternative: Literal["two-sided", "smaller", "larger"] = "two-sided",
+) -> Dict[str, np.ndarray]:
+    if control.ndim == 3:
+        control = triangle_to_vector(control)
+    if diagnosed.ndim == 3:
+        diagnosed = triangle_to_vector(diagnosed)
+
+    control_means = control.mean(axis=0, where=~np.isnan(control))
+    control_vars = control.var(axis=0, where=~np.isnan(control))
+    control_lens = (~np.isnan(control)).sum(0)
+
+    diagnosed_means = diagnosed.mean(axis=0, where=~np.isnan(diagnosed))
+    diagnosed_vars = diagnosed.var(axis=0, where=~np.isnan(diagnosed))
+    diagnosed_lens = (~np.isnan(diagnosed)).sum(0)
+
+    dof = (control_vars / control_lens + diagnosed_vars / diagnosed_lens) ** 2 / (
+        (control_vars / control_lens) ** 2 / (control_lens - 1)
+        + (diagnosed_vars / diagnosed_lens) ** 2 / (diagnosed_lens - 1)
+    )
+    t_vals = (control_means - diagnosed_means) / np.sqrt(
+        control_vars / control_lens + diagnosed_vars / diagnosed_lens
+    )
+
+    if alternative == "smaller":
+        p_vals = stats.t.cdf(t_vals, df=dof)
+    elif alternative == "larger":
+        p_vals = stats.t.sf(t_vals, df=dof)
+    elif alternative == "two-sided":
+        p_vals = 2 * stats.t.sf(np.abs(t_vals), df=dof)
+    else:
+        raise ValueError(
+            f"alternative should be one of ['two-sided', 'smaller', 'larger'], "
+            f"got {alternative} instead"
+        )
+
+    if p_adjust_method is not None:
+        from statsmodels.stats.multitest import multipletests
+
+        p_vals_adjusted = multipletests(pvals=p_vals, method=p_adjust_method)
+    else:
+        p_vals_adjusted = np.full_like(p_vals, np.nan)
+
+    return {
+        "control_means": control_means,
+        "control_vars": control_vars,
+        "control_lens": control_lens,
+        "diagnosed_means": diagnosed_means,
+        "diagnosed_vars": diagnosed_vars,
+        "diagnosed_lens": diagnosed_lens,
+        "t_vals": t_vals,
+        "dof": dof,
+        "p_vals": p_vals,
+        "p_vals_adjusted": p_vals_adjusted,
+    }
